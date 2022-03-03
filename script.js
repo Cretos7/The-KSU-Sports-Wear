64d181b8ef92a85478716014c762408cbfd9c0a8
Dylan Hoeffner <100368683+Cretos7@users.noreply.github.com>
2022-03-03 15:01:10 +0000

    added gloves object and interface

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ var shoe1={
   shoeName:""
 };
 
+var gloves1={
+  color:"",
+  size:"",
+  season:"",
+  material:""
+};
+
 function checkOut() {
   shoe1.color=document.getElementById("colorInput").value;
   shoe1.size=document.getElementById("sizeInput").value;
   shoe1.shoeName=document.getElementById("shoeNameInput").value;
   alert(shoe1.color + " " + shoe1.size + " " + shoe1.shoeName);
+}
+
+function gloveRequest(){
+  gloves1.color=document.getElementById("gloveColor").value;
+  gloves1.size=document.getElementById("gloveSize").value;
+  gloves1.season=document.getElementById("gloveSeason").value;
+  gloves1.material=document.getElementById("gloveMaterial").value;
+  
 }
